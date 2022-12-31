ec068d80b9ca0f68323d2ab32fa070c692d14ea1
Dan Kendall <82474590+bitofastickler@users.noreply.github.com>
2022-12-30 19:30:36 -0500

    Create insert_data.sh
    inserts data from .csv

---
+++ insert_data.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    #get winner_id
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+
+    #if not found
+    if [[ -z $WINNER_ID ]]
+    then
+      #insert winner
+      INSERT_WINNER=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER') ON CONFLICT DO NOTHING")
+      if [[ $INSERT_WINNER == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $WINNER
+      fi
+    fi
+
+    #get opponent_id
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    #if not found
+    if [[ -z $OPPONENT_ID ]]
+    then
+      #insert opponent
+      INSERT_OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT') ON CONFLICT DO NOTHING")
+      if [[ $INSERT_OPPONENT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $OPPONENT
+      fi
+    fi
+  fi
+  if [[ $YEAR != "year" ]]
+  then
+    #get game_id
+    GAME_ID=$($PSQL "SELECT game_id FROM games WHERE year=$YEAR AND round='$ROUND' AND winner_id='$WINNER'")
+
+    #if game_id not found
+    if [[ -z $GAME_ID ]]
+    then
+      WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")"
+      OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")" 
+      #Insert games
+      INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES('$YEAR', '$ROUND', '$WINNER_ID', '$OPPONENT_ID', '$WINNER_GOALS', '$OPPONENT_GOALS') ON CONFLICT DO NOTHING")
+  
+      if [[ $INSERT_GAME_RESULT == 'INSERT 0 1' ]]
+      then
+        echo Inserted $INSERT_GAME_RESULT
+      fi
+    fi
+  fi
+done
+
+
+    
